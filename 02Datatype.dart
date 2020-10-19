967b7c6fdcafd3dbe42c8f82a7c75097c1a38806
RoopsBhasima7 <rupeshbhasima@gmail.com>
2020-10-19 20:08:00 +0545

    Dart

---
+++ 02Datatype.dart
@@ -0,0 +1,10 @@
+void main(){
+  String name = "Rupesh"; //String
+  String address ="Bhaktapur";
+  int age = 23; //Int
+  double salary = 150000.00;
+  bool isMarried = false;
+  DateTime dob = DateTime(1997,06,02);
+  List<String> programmingLanguage = ['C','C++','Java','Dart','Python'];
+  print("My name is ${name}. I am from ${address}. My age is ${age}. My salary is ${salary}. I am married = ${isMarried}\n.My DOB is ${dob.year}.");
+}
---
+++ 03List.dart
@@ -0,0 +1,5 @@
+void main(){
+  List<String> programmingLanguage = ['C','C++','Java','Dart','Python'];
+  List<int> price = [100,200,300,500,400];
+  print(programmingLanguage[1]);
+  }
