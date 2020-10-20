f0173125ab72ba84e1cad1dc4a39d4b67aaf53d2
RoopsBhasima7 <rupeshbhasima@gmail.com>
2020-10-20 19:44:59 +0545

    Dart

--- 07Condition.dart
+++ 07Condition.dart
@@ -1,1 +1,20 @@
-// Condition 
+// Voter or Non-Voter 
+// || => OR &&
+void main(){
+  int age = 36;
+  // if(age>=20){
+  //   print("Voter");
+  // }else{
+  //   print("Non-Voter");
+  // }
+
+  if(age<10){
+    print("You are child.");
+  }else if(age<20){
+    print("You are youth.");
+  }else if(age<35){
+    print("You are Matured.");
+  }else{
+    print("You are Old.");
+  }
+}
