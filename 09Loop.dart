f214a1cc1027f71f72b7ffbfa44aa97aebce5b53
RoopsBhasima7 <rupeshbhasima@gmail.com>
2020-10-21 20:10:09 +0545

    Dart

---
+++ 09Loop.dart
@@ -0,0 +1,16 @@
+//Loop : Repeat Something
+/*
+Types :
+1. For Loop
+2. while loop
+3.do while loop
+4.for each loop
+*/
+void main(){
+// for(int i=1; i<=100; i++){
+// print("Rupesh Bhasima");
+// }
+for(int i=100; i>=1; i--){
+print(i);
+}
+}
