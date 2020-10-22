2d39b8867599dabe28880594b50c1e00ea940f41
RoopsBhasima7 <rupeshbhasima@gmail.com>
2020-10-22 18:57:32 +0545

    Dart

---
+++ 13Function.dart
@@ -0,0 +1,17 @@
+void main(){
+tarkariLiyeraAaune();
+singasong();
+}
+
+void tarkariLiyeraAaune(){
+  print("First Get Up:");
+  print("Make a list of tarkari");
+  print("Start a bike");
+  print("Shop the tarkari");
+  print("Go to home");
+}
+
+void singasong(){
+  print("Start Track");
+  print("just sing it");
+}
---
+++ 14AdvanceFunction.dart
@@ -0,0 +1,19 @@
+/*
+Function Types
+1.No Return Type and No Parameter
+2.Return Type and No Parameter
+3.Return Type and Parameter
+4.No return Type and Parameter
+*/
+void main(){
+String age = defaultName();
+print(age);
+}
+
+int showDefaultAge(){
+  return 1;
+}
+
+String defaultName(){
+  return "Rupesh";
+}
