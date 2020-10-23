2b76be220cf3aca45dc77770703a52b652ef26ae
RoopsBhasima7 <rupeshbhasima@gmail.com>
2020-10-23 16:10:14 +0545

    Dart

---
+++ 15LoopInMap.dart
@@ -0,0 +1,11 @@
+void main(){
+  var courses = {
+    "C" : 1000,
+    "Java" : 2000,
+    "Flutter" : 5000
+  };
+
+  courses.forEach((k, v) {
+  print("$k costs total Rs. $v");
+   });
+}
---
+++ 16OOP.dart
@@ -0,0 +1,19 @@
+// OOP
+class Add{
+  //Prop
+  int a;
+  int b;
+
+  //Constructor
+  Add(this.a,this.b);
+
+  //Getter Setter
+  //Method/Function
+  void add(){
+    print(a+b);
+  }
+}
+
+void main(){
+  
+}
