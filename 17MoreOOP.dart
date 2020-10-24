6756140c24bbb6b5adbc994d8c5934c80e315c5d
RoopsBhasima7 <rupeshbhasima@gmail.com>
2020-10-24 19:34:13 +0545

    Dart

--- 17MoreOOP.dart
+++ 17MoreOOP.dart
@@ -7,7 +7,19 @@ class Human{
   bool isMarried;
 
   //Ctor
-  Human(this.id,this.name,this.address,this.birthdate,this.isMarried);
+  //Human(this.id,this.name,this.address,this.birthdate,this.isMarried);
+  Human(int id,String name,String address, DateTime birthdate, bool isMarried){
+    this.id = id;
+    if(name==""){
+      this.name="No Name";
+    }else{
+      this.name = name;
+    }
+    this.address = address;
+    this.birthdate = birthdate;
+    this.isMarried = isMarried;
+  }
+
 
   void showHumanDetails(){
     print("ID is $id. Name is $name, lives in $address,birthdate is $birthdate. Married: $isMarried");
@@ -15,7 +27,7 @@ class Human{
 }
 
 void main(){
-  Human rupesh = Human(1, "Rupesh","Bhaktapur",DateTime(1997-15-14),false);
+  Human rupesh = Human(1, "","Bhaktapur",DateTime(1997-15-14),false);
   Human samana = Human(1, "Samana","Bhaktapur",DateTime(2020-14-14),false);
 
   rupesh.showHumanDetails();
