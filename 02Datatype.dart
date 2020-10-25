ffacea8409d80cb64fa1eda164cbec1a52ef4e71
RoopsBhasima7 <rupeshbhasima@gmail.com>
2020-10-25 18:11:22 +0545

    Dart

--- 02Datatype.dart
+++ 02Datatype.dart
@@ -5,6 +5,5 @@ void main(){
   double salary = 150000.00;
   bool isMarried = false;
   DateTime dob = DateTime(1997,06,02);
-  List<String> programmingLanguage = ['C','C++','Java','Dart','Python'];
   print("My name is ${name}. I am from ${address}. My age is ${age}. My salary is ${salary}. I am married = ${isMarried}\n.My DOB is ${dob.year}.");
 }
